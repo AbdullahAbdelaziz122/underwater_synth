3adc59b1d6d0bdd753de5ad44e2d015565e3f584
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-06-08 14:33:44 +0300

    fix

--- Models/inference/main.py
+++ Models/inference/main.py
@@ -41,6 +41,8 @@ from pathlib import Path
 from typing import Dict, Any
 
 import torch
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
 from inference import HierarchicalClassifier
 
 # ---------------------------------------------------------------------------
